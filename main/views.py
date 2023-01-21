666fc90f7f312676b3bc53ee9857746a40aaac61
Chan Weng Ho <cwengho@yahoo.com>
2023-01-22 01:17:47 +0800

    initial base template and views

--- main/views.py
+++ main/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.http.response import HttpResponseRedirect
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+
 
 # Create your views here.
+def welcome(response):
+    return render(response, 'main/welcome.html', {})
