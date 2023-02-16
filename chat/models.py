d585c5c4b764cec99b94db209e09b9315bcdc85f
Chan Weng Ho <cwengho@yahoo.com>
2023-02-17 00:34:27 +0800

    adds functionality that creates chatroom between tutor and parent when parent requests for tutor

--- chat/models.py
+++ chat/models.py
@@ -2,13 +2,26 @@ from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+class ChatRoom (models.Model):
+    tutor = models.ForeignKey(User, related_name='tutor', on_delete=models.CASCADE, null=True)
+    parent = models.ForeignKey(User, related_name='parent', on_delete=models.CASCADE, null=True)
+
+    def last_10_messages(self):
+        return self.message_set.order_by('-timestamp')[:10]
+
+
 class Message (models.Model):
     author = models.ForeignKey(User, related_name='author_messages', on_delete=models.CASCADE)
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
+    chat_room = models.ForeignKey(ChatRoom, on_delete=models.CASCADE, null=True)
 
     def __str__(self):
         return self.author.username
 
-    def last_10_messages():
-        return Message.objects.order_by('-timestamp').all()[:10]
+
+    
+
+
+    
