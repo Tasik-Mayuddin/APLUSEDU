8f1dff336a02ce7b3895bd70a7cad93604ab5914
Chan Weng Ho <cwengho@yahoo.com>
2023-03-30 21:33:29 +0800

    switch to sqllite for now

--- APLUSEDU/settings.py
+++ APLUSEDU/settings.py
@@ -106,17 +106,17 @@ CHANNEL_LAYERS = {
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'old': {
+    'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     },
-    'default': {
-    'ENGINE': 'django.db.backends.mysql',
-    'NAME': 'aplusedu',
-    'USER': 'root',
-    'PASSWORD': 'bester123',
-    'HOST': '127.0.0.1',
-    'PORT': '3306',
+    'mysql': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'aplusedu',
+        'USER': 'root',
+        'PASSWORD': 'bester123',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     },
 }
 
