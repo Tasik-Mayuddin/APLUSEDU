46dc984c54e54c79762929194c0c291f9c7f31c1
Chan Weng Ho <cwengho@yahoo.com>
2023-04-03 18:13:25 +0800

    fixed tutor request appearance, improved styling of subjectandlevel details

--- frontend/src/components/Tutor/Requests.js
+++ frontend/src/components/Tutor/Requests.js
@@ -1,8 +1,18 @@
 import RequestIndie from "./RequestIndie"
-import React from "react"
+import React, { useEffect, useState } from "react"
 
 
 const Requests = ({ availability, onAccept, onDecline }) => {
+
+    const [blankRequest, setBlankRequest] = useState(true)
+
+    useEffect(()=>{
+        setBlankRequest(true)
+        availability.dayandtime_set.forEach(({ bookedslot_set }) => {
+            bookedslot_set.filter(bslot=>bslot.status==="pending").length&&setBlankRequest(false)
+        })
+    }, [availability, onAccept, onDecline])
+
     const noRequests = () => {
         return (
             <div className="no-requests">
@@ -11,24 +21,22 @@ const Requests = ({ availability, onAccept, onDecline }) => {
         )
     }
 
-    return (
+    return (                
         <>
                 {/* get list of teacher's free time */}
-                {availability.hasOwnProperty("dayandtime_set")?availability.dayandtime_set.map((item, id)=>(
-                    <React.Fragment key={id}>
-                        {/* get inner list of pending bookedslots */}
-                        {console.log(item.bookedslot_set.filter(bslot=>bslot.status==="pending"))}
-                        {item.bookedslot_set.filter(bslot=>bslot.status==="pending").length?
-                            item.bookedslot_set.filter(bslot=>bslot.status==="pending").map((bslot, id)=>(
-                                <RequestIndie key={id} request={bslot} day={item.day} onAccept={onAccept} onDecline={onDecline} />
-                            )):
-                            noRequests()
-                        }
-                    </React.Fragment>       
-                )):
-                noRequests()
+                {availability.dayandtime_set.map((item, id)=>{
+                    return (
+                        <React.Fragment key={id}>
+                            {/* get inner list of pending bookedslots */}
+                            {item.bookedslot_set.filter(bslot=>bslot.status==="pending").map((bslot, id)=>(
+                                    <RequestIndie key={id} request={bslot} day={item.day} onAccept={onAccept} onDecline={onDecline} />
+                                ))
+                            }
+                        </React.Fragment>       
+                    )})
                 }
-            </>
+                {blankRequest&&noRequests()}
+        </>
     )
 }
 
