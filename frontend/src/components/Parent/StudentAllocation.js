e14d9c722f9a8eca8517666f1330fe4642e6bca4
Chan Weng Ho <cwengho@yahoo.com>
2023-04-10 17:12:38 +0800

    added ability to delete request/allocations from parent POV

--- frontend/src/components/Parent/StudentAllocation.js
+++ frontend/src/components/Parent/StudentAllocation.js
@@ -2,13 +2,16 @@ import { Link } from "react-router-dom"
 import { formatTime } from "../../functions"
 import ButtonSmall from "../Buttons/ButtonSmall"
 import { BsFillChatDotsFill } from "react-icons/bs";
+import { RxCross1 } from "react-icons/rx";
 
-const StudentAllocation = ({ allocationDetails }) => {
-	
+const StudentAllocation = ({ allocationDetails, onDelete }) => {
 
   return (
     <div className={`student-allocation${allocationDetails.status==='approved'?` student-allocation-approved`:''}`} >
-      <h3>{allocationDetails.day}, {formatTime(allocationDetails.start_time)} - {formatTime(allocationDetails.end_time)}</h3>
+      <div className="header" style={{display: 'flex', justifyContent: 'space-between'}}>
+        <h3 style={{margin: 'auto 0'}}>{allocationDetails.day}, {formatTime(allocationDetails.start_time)} - {formatTime(allocationDetails.end_time)}</h3>
+        <button className='icon-button' onClick={()=>onDelete({bSlotId: allocationDetails.bSlotId})}><RxCross1 size={'24px'}/></button>
+      </div>
       <h4>{allocationDetails.subject} with Teacher {allocationDetails.tutor}</h4>
       <div style={{display: 'flex', justifyContent: 'space-between'}}>
         <p style={{margin: 'auto 0'}}>
