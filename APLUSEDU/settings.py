eaae9f9d15f872964537a9183889098fd1454a20
Chan Weng Ho <cwengho@yahoo.com>
2023-04-14 15:45:08 +0800

    tweaked settings

--- APLUSEDU/settings.py
+++ APLUSEDU/settings.py
@@ -166,23 +166,22 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "frontend/build/static"),
 ]
 
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-# MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
-AWS_ACCESS_KEY_ID = ''
-AWS_SECRET_ACCESS_KEY = ''
-AWS_STORAGE_BUCKET_NAME = 'tasik-aws'
+# AWS_ACCESS_KEY_ID = ''
+# AWS_SECRET_ACCESS_KEY = ''
+# AWS_STORAGE_BUCKET_NAME = 'tasik-aws'
 
-AWS_S3_FILE_OVERWRITE = False
+# AWS_S3_FILE_OVERWRITE = False
 
-AWS_S3_REGION_NAME = 'ap-southeast-1' # e.g. us-east-1
-AWS_S3_CUSTOM_DOMAIN = '%s.s3-%s.amazonaws.com' % (AWS_STORAGE_BUCKET_NAME, AWS_S3_REGION_NAME)
-AWS_QUERYSTRING_AUTH = False
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-# STORAGES = {"default": "storages.backends.s3boto3.S3Boto3Storage"}
+# AWS_S3_REGION_NAME = 'ap-southeast-1' # e.g. us-east-1
+# AWS_S3_CUSTOM_DOMAIN = '%s.s3-%s.amazonaws.com' % (AWS_STORAGE_BUCKET_NAME, AWS_S3_REGION_NAME)
+# AWS_QUERYSTRING_AUTH = False
+# DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-MEDIA_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
-MEDIA_ROOT = ''
+# MEDIA_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
+# MEDIA_ROOT = ''
 
 CORS_ORIGIN_WHITELIST = [
     'http://localhost:3000',
